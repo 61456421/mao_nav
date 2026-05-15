4350e282969febbba71dd9bcbdbb544174dd0483
61456421 <61456421@qq.com>
2026-05-15 15:22:10 +0800

    Update category icon path in mock data

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -2,7 +2,7 @@ export const mockData = {
   "categories": [
     {
       "id": "category-1778812347550",
-      "icon": "📁",
+      "icon": "/sitelogo/gu.ico",
       "name": "鑫鑫学习",
       "order": 0,
       "sites": [
